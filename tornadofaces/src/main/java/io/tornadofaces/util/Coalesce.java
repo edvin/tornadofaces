268850a7845d3219cc9f57df969bf4db1f81bb4b
edvin <es@syse.no>
2015-08-04 12:07:25 +0200

    Added coalesceString function tag

--- tornadofaces/src/main/java/io/tornadofaces/util/Coalesce.java
+++ tornadofaces/src/main/java/io/tornadofaces/util/Coalesce.java
@@ -1,5 +1,7 @@
 package io.tornadofaces.util;
 
+import java.util.Arrays;
+
 public class Coalesce {
 	public static <T> T coalesce(T... values) {
 		for (T v : values)
@@ -8,8 +10,8 @@ public class Coalesce {
 		return null;
 	}
 
-	public static String coalesceString(Object... values) {
-		for (Object v : values)
+	public static String coalesceString(Object v1, Object v2) {
+		for (Object v : Arrays.asList(v1, v2))
 			if (v != null && !"".equals(v.toString()))
 				return v.toString();
 		return null;
